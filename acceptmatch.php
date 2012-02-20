ecaae5aa5bd82b31249be63f87c03a28dc1dccd5
Jonathan Koff <jonathankoff@gmail.com>
2012-02-20 08:33:47 +0000

    fixed match accept problem

--- acceptmatch.php
+++ acceptmatch.php
@@ -1,15 +1,16 @@
 <?php
 
 $token = $_GET['token'];
-$query = "UPDATE partials SET active=0 " .
-         "WHERE MD5(CONCAT(\"dfsnib9\", id))=\"$token\"";
-$result = mysql_query($query) or die(mysql_error());
 
 $query = "SELECT users.*, partials.activity_id FROM partials " .
     "LEFT JOIN users ON users.id = partials.matched_user_id " .
     "WHERE MD5(CONCAT(\"dfsnib9\", partials.id))=\"$token\" AND partials.active=1";
 $result = mysql_query($query) or die(mysql_error());
 $row = mysql_fetch_assoc($result);
+
+$query = "UPDATE partials SET active=0 " .
+         "WHERE MD5(CONCAT(\"dfsnib9\", id))=\"$token\"";
+$result = mysql_query($query) or die(mysql_error());
 
 $to = $row['email'] . ", " . $user->email;
 $subject = "You have been matched!";
