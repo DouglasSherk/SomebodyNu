54c7128d7fd56b367d3267aac16f87db20ffe86b
Jonathan Koff <jonathankoff@gmail.com>
2012-02-20 08:44:40 +0000

    incomplete commit

--- acceptmatch.php
+++ acceptmatch.php
@@ -8,7 +8,7 @@ $query = "SELECT users.*, partials.activity_id FROM partials " .
 $result = mysql_query($query) or die(mysql_error());
 $row = mysql_fetch_assoc($result);
 
-$query = "UPDATE partials SET active=0 " .
+$query = "DELETE FROM partials " .
          "WHERE MD5(CONCAT(\"dfsnib9\", id))=\"$token\"";
 $result = mysql_query($query) or die(mysql_error());
 
@@ -17,8 +17,8 @@ $subject = "You have been matched!";
 $tmpl = 'email';
 include_once("email/send.php");
 
-$query = "DELETE FROM queues WHERE (user_id='$row[id]' OR user_id='$user->id') AND activity_id='$activity_id';";
-mysql_query($query);
+$query = "DELETE FROM queues WHERE (user_id='" . $row['id'] . "' OR user_id='$user->id') AND activity_id='$activity_id';";
+mysql_query($query) or die(mysql_error());
 
 $_SESSION['matched_name'] = $row['name'];
 $_SESSION['matched_uid'] = $row['uid'];
