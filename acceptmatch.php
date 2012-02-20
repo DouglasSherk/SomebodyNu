893e9ee04f9d0d3a33c7e847e492ca49432cf75f
Jonathan Koff <jonathankoff@gmail.com>
2012-02-20 08:12:48 +0000

    unqueue on match

--- acceptmatch.php
+++ acceptmatch.php
@@ -16,6 +16,9 @@ $subject = "You have been matched!";
 $tmpl = 'email';
 include_once("email/send.php");
 
+$query = "DELETE FROM queues WHERE (user_id='$row[id]' OR user_id='$user->id') AND activity_id='$activity_id';";
+mysql_query($query);
+
 $_SESSION['matched_name'] = $row['name'];
 $_SESSION['matched_uid'] = $row['uid'];
 
