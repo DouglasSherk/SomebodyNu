ccc8b018b782d4f4185e3dc1d7ead373a0c5474b
Jonathan Koff <jonathankoff@gmail.com>
2012-03-28 09:25:00 +0000

    migration script

--- setup/update.php
+++ setup/update.php
@@ -21,7 +21,7 @@ mysql_query($query) or die(mysql_error());
 $query = "DROP TABLE IF EXISTS partials (" .
 mysql_query($query) or die(mysql_error());*/
 
-mysql_query('DROP TABLE partials');
+/*mysql_query('DROP TABLE partials');
 $query = "CREATE TABLE partials (" .
          "id SERIAL, " . 
          "user_id BIGINT NOT NULL, " .
@@ -33,4 +33,40 @@ $query = "CREATE TABLE partials (" .
          "FOREIGN KEY (matched_user_id) REFERENCES users(id) ON DELETE CASCADE, " .
          "PRIMARY KEY(id), " .
          "UNIQUE KEY(user_id, matched_user_id));";
+mysql_query($query) or die(mysql_error());*/
+
+mysql_query('DELETE FROM queues;') or die(mysql_error());
+
+$query = "DROP TABLE IF EXISTS activities;";
+mysql_query($query) or die(mysql_error());
+$query = "CREATE TABLE activities (" .
+         "id SERIAL, " .
+         "name VARCHAR(255) NOT NULL, " .
+         "default_size INT NOT NULL, " .
+         "PRIMARY KEY(id), " .
+         "UNIQUE KEY(name));";
+mysql_query($query) or die(mysql_error());
+
+$query = "DROP TABLE IF EXISTS groups;";
+mysql_query($query) or die(mysql_error());
+$query = "CREATE TABLE groups (" .
+         "id SERIAL," .
+         "activity_id BIGINT NOT NULL, " .
+         "location VARCHAR(255) NOT NULL, " .
+         "size BIGINT NOT NULL, " .
+         "time_created TIMESTAMP DEFAULT NOW(), " .
+         "FOREIGN KEY (activity_id) REFERENCES activities(id) ON DELETE CASCADE, " .
+         "PRIMARY KEY(id));";
+mysql_query($query) or die(mysql_error());
+
+$query = "DROP TABLE IF EXISTS group_members;";
+mysql_query($query) or die(mysql_error());
+$query = "CREATE TABLE group_members( " .
+         "id SERIAL," .
+         "user_id BIGINT NOT NULL, " .
+         "group_id BIGINT NOT NULL, " .
+         "FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, " .
+         "FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE, " .
+         "PRIMARY KEY(id)," .
+         "UNIQUE KEY(user_id, group_id));";
 mysql_query($query) or die(mysql_error());
