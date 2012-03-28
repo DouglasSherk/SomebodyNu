7b2fc42cf27eb553abf1d46d2620e25e96599715
Doug Sherk <github@sherk.me>
2012-03-28 16:52:53 +0000

    fixed activities through update.php script

--- setup/update.php
+++ setup/update.php
@@ -3,34 +3,22 @@ chdir("..");
 require_once("includes/config.php");
 Database::connect();
 
-/*$query = "DROP TABLE IF EXISTS analytics;";
+$query = "DROP TABLE IF EXISTS activities;";
 mysql_query($query) or die(mysql_error());
-$query = "CREATE TABLE analytics (" .
+$query = "CREATE TABLE activities (" .
          "id SERIAL, " .
-         "counter VARCHAR(128), " .
-         "kingdom VARCHAR(128), " .
-         "phylum VARCHAR(128), " .
-         "class VARCHAR(128), " .
-         "family VARCHAR(128), " .
-         "genus VARCHAR(128), " .
-         "value INT(11), " .
-         "time TIMESTAMP DEFAULT NOW()" .
-         ");";
-mysql_query($query) or die(mysql_error());
-
-$query = "DROP TABLE IF EXISTS partials (" .
-mysql_query($query) or die(mysql_error());*/
-
-mysql_query('DROP TABLE partials');
-$query = "CREATE TABLE partials (" .
-         "id SERIAL, " . 
-         "user_id BIGINT NOT NULL, " .
-         "matched_user_id BIGINT NOT NULL, " .
-         "activity_id BIGINT NOT NULL, " .
-         "active INTEGER NOT NULL DEFAULT 1," .
-         "time_created TIMESTAMP DEFAULT NOW(), " .
-         "FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, " .
-         "FOREIGN KEY (matched_user_id) REFERENCES users(id) ON DELETE CASCADE, " .
+         "name VARCHAR(255) NOT NULL, " .
+         "default_size INT NOT NULL, " .
          "PRIMARY KEY(id), " .
-         "UNIQUE KEY(user_id, matched_user_id));";
+         "UNIQUE KEY(name));";
 mysql_query($query) or die(mysql_error());
+
+$data = file_get_contents('setup/activities.csv');
+$tok = strtok($data, "\n");
+while ($tok !== false) {
+    $fields = explode(',', $tok);
+    $query = "INSERT INTO activities (name, default_size) " .
+             "VALUES(\"" . $fields[0] . "\", \"" . $fields[1] . "\");";
+    mysql_query($query) or die(mysql_error());
+    $tok = strtok("\n");
+}
